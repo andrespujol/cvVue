fedadc9d3e16b5d689501e4b46a28144132ae3d5
Andrés Pujol <andrespujol@gmail.com>
2021-12-08 23:56:17 -0300

    actualicé formulario

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,17 @@
 <template>
-    <b-container fluid>
-        <b-row class="my-1" v-for="type in types" :key="type">
-        <b-col sm="3">
-            <label :for="`type-${type}`"><code>{{ type }}</code>:</label>
-        </b-col>
-        <b-col sm="9">
-            <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
-        </b-col>
-        </b-row>
-        <b-button type="submit" variant="primary" class="btnLogin">Entrar</b-button>
-    </b-container>
+    <b-form>
+        <b-container fluid>
+            <b-row class="my-1" v-for="type in types" :key="type">
+            <b-col sm="3">
+                <label :for="`type-${type}`"><code>{{ type }}</code>:</label>
+            </b-col>
+            <b-col sm="9">
+                <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
+            </b-col>
+            </b-row>
+            <b-button type="submit" variant="primary" class="btnLogin">Entrar</b-button>
+        </b-container>
+    </b-form>
 
 </template>
 
@@ -19,6 +21,8 @@
     data() {
         return {
             types: [
+                'usuario',
+                'nombre',
                 'email',
                 'password',
             ]
@@ -29,6 +33,7 @@
             event.preventDefault()
             alert(JSON.stringify(this.form))
     },
+
 
     }
 }
